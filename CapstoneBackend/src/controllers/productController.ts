989bfc309c2cb80a9b0b82aeb0045987b9651e04
MohanPutreddy <pmohankrishnareddy@gmail.com>
2024-03-06 01:10:13 -0500

    Getting book by Id API endpoint added in the backend and style updates to product-card image.

--- CapstoneBackend/src/controllers/productController.ts
+++ CapstoneBackend/src/controllers/productController.ts
@@ -56,3 +56,25 @@ export const getProducts = async (
     products,
   });
 };
+
+export const getProductById = async (
+  req: CustomRequest,
+  res: Response,
+  next: NextFunction
+
+) => {
+  try {
+    const { id } = req.params; // Extract the ID from the request parameters
+    const product = await prisma.products.findUnique({
+      where: { id: parseInt(id) },
+    });
+    if (product) {
+      res.json({ status: true, product });
+    } else {
+      res.status(404).json({ status: false, message: "Product not found" });
+    }
+  } catch (error) {
+    console.error("Failed to fetch product:", error);
+    res.status(500).json({ status: false });
+  }
+};
