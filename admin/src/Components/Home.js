efa546930350963a70a3079122b1ce25dbfebf5a
MohanPutreddy <pmohankrishnareddy@gmail.com>
2024-04-18 11:25:38 -0400

    Admin Home page update

--- admin/src/Components/Home.js
+++ admin/src/Components/Home.js
@@ -1,9 +1,13 @@
 import React from "react";
+import bannerImage from "../Images/banner_image.jpg";
 
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <div className="image-container">
+        <img src={bannerImage} alt="Banner" className="width100" />
+        <div className="overlay">Welcome to Admin View</div>
+      </div>
     </div>
   );
 }
